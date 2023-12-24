0fe7cc02dcb7391fec8cb337d029d1f8d987561a
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2023-12-24 12:24:31 +0900

    認証機能の追加

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,16 @@
 import axios from 'axios';
 
+const headers = {
+  "Content-type": "Application/json",
+  "Accept": "application/json",
+  "X-Requested-With": "XMLHttpRequest"
+};
+
 const instance = axios.create({
+  baseURL: "http://localhost:8000/",
   withCredentials: true,
-  xsrfCookieName: 'XSRF-TOKEN',
-  baseURL: 'http://localhost:8000',
-  timeout: 1000,
+  withXSRFToken: true,
+  headers: headers,
 });
+
 export default instance;
