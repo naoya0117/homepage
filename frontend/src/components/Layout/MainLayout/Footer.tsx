916fbacb9ccfb04500b145d5fd20579e9a5eaf74
naoya0117 <mmn117302@outlook.jp>
2023-12-31 00:17:23 +0900

    homeを更新

--- frontend/src/components/Layout/MainLayout/Footer.tsx
+++ frontend/src/components/Layout/MainLayout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="h-30v flex items-center justify-center mt-5 mb-5">
+    <footer className="h-30v flex items-center justify-center py-10 bg-black text-white">
       <p>© 2023 naoya0117. All rights reserved.</p>
     </footer>
   );
--- frontend/src/components/Layout/MainLayout/Navbar.tsx
+++ frontend/src/components/Layout/MainLayout/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, MenuItem, useMediaQuery, useTheme, Drawer } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Button, MenuItem, useMediaQuery, useTheme, Drawer, Menu, Dialog, DialogTitle, DialogContent } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from 'react-router-dom';
-
+import { LoginForm } from '@/features/auth';
 const Navbar = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
     setDrawerOpen(true);
@@ -15,6 +18,22 @@ const Navbar = () => {
 
   const handleDrawerClose = () => {
     setDrawerOpen(false);
+  };
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
   };
 
   const items = [
@@ -25,7 +44,7 @@ const Navbar = () => {
 
   return (
     <div style={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="default">
+      <AppBar position="fixed" style={{ backgroundColor: 'black' }}>
         <Toolbar>
           {isMobile && (
             <IconButton edge="start" style={{ marginRight: '16px' }} color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
@@ -38,8 +57,26 @@ const Navbar = () => {
           {!isMobile && items.map((item) => (
             <Button color="inherit" href={item.href}>
               <Link to={item.href}>{item.name}</Link>
-              </Button>
+            </Button>
           ))}
+          <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <MoreVertIcon style={{ color: 'white' }} />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClickOpen}>Login</MenuItem>
+          </Menu>
+          <Dialog open={open} onClose={handleCloseDialog}>
+            <DialogTitle>Login</DialogTitle>
+            <DialogContent>
+              <LoginForm />
+            </DialogContent>
+          </Dialog>
         </Toolbar>
       </AppBar>
       <Drawer open={drawerOpen} onClose={handleDrawerClose}>
