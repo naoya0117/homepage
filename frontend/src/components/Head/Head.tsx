89d755667389e878c568f6f261b639d02df22d96
naoya0117 <mmn117302@outlook.jp>
2024-01-02 09:43:06 +0900

    画像の更新

--- frontend/src/components/Head/Head.tsx
+++ frontend/src/components/Head/Head.tsx
@@ -17,7 +17,7 @@ export const Head = ({title = '', description = ''}:HeadProps= {}) => {
         {property: 'og:description', content: description},
         {property: 'og:type', content: 'website'},
         {property: 'og:site_name', content: "Naoya's portfolio"},
-        {property: 'og:image', content: 'https://naoya.com/logo.png'},
+        {property: 'og:image', content: 'https://naoya0117.com/logo.png'},
       ]}
     >
     </Helmet>
