11fc402c0f3ee20295e71460ffbf58df7eeb1113
naoya0117 <81454816+naoya0117@users.noreply.github.com>
2024-04-04 02:07:44 +0900

    fix: whitelistの追加

--- backend/app/Http/Middleware/WhitelistIPs.php
+++ backend/app/Http/Middleware/WhitelistIPs.php
@@ -3,24 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
-class WhitelistIPs
+class WhitelistHostnames
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        // 許可されたIPアドレスのリスト
-        $allowedIPs = explode(',', config('app.whitelist_ips'));
+        // 許可されたホスト名のリストを.envファイルから取得
+        $allowedHostnames = explode(',', config('app.whitelist_hostnames'));
 
-        // リクエストのIPアドレスを取得
-        $requestIP = $request->ip();
+        // リクエストのホスト名を取得
+        $requestHostname = $request->getHost();
 
-        // リクエストのIPアドレスが許可されたリストに含まれているかを確認
-        if (!in_array($requestIP, $allowedIPs)) {
-            // 許可されたIPアドレスでない場合は、403 Forbiddenのレスポンスを返す
+        // リクエストのホスト名が許可されたリストに含まれているかを確認
+        if (!in_array($requestHostname, $allowedHostnames)) {
+            // 許可されたホスト名でない場合は、403 Forbiddenのレスポンスを返す
             return response()->json(['error' => 'Access Denied'], 403);
         }
 
-        // 許可されたIPアドレスの場合は、次のミドルウェアに処理を渡す
+        // 許可されたホスト名の場合は、次のミドルウェアに処理を渡す
         return $next($request);
     }
 }
